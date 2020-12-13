c120aee1cd00824c0c89bfc9e51602a2362757e3
Maksim Stseshanok <maksim.stseshanok@gmail.com>
2020-12-14 02:47:49 +0300

    feat: rebuild project

--- src/getDom.js
+++ src/getDom.js
@@ -1,31 +1,48 @@
 export {getDom}
 
 function getDom(){
+    const root = document.querySelector('#root');
 
-    let globalCasesCounter = document.createElement('div');
+    const header = document.createElement('header');
+    header.classList.add('header');
+    
+    const map = document.createElement('div');
+    map.classList.add('map')
+
+    const wrapper = document.createElement('div');
+    wrapper.classList.add('wrapper');
+
+    const wrapperInner = document.createElement('div');
+    wrapperInner.classList.add('wrapper__inner');
+
+    const globalCasesCounter = document.createElement('div');
     globalCasesCounter.classList.add('global-case-counter');
 
-    let divSearch = document.createElement('input');
+    const divSearch = document.createElement('input');
     divSearch.classList.add('data-search');
     divSearch.placeholder = 'введите название страны';
 
-    let dataArticle = document.createElement('article');
+    const dataArticle = document.createElement('article');
     dataArticle.classList.add('data-article');
 
-    let divData = document.createElement('div');
+    const divData = document.createElement('div');
     divData.classList.add('data-countries');
 
-    let divStatistics = document.createElement('div');
+    const divStatistics = document.createElement('div');
     divStatistics.classList.add('data-statistics');
 
-    let divSelect = document.createElement('div');
+    const divSelect = document.createElement('div');
     divSelect.classList.add('container-choice-indicator');
 
-    let choiceIndicator = document.createElement('select');
+    const choiceIndicator = document.createElement('select');
     choiceIndicator.classList.add('choice-indicator');
 
-    let scheduleMain = document.createElement('div');
+    const scheduleMain = document.createElement('div');
     scheduleMain.classList.add('schedule-main');
+
+    header.innerHTML = `
+        <h1 class="header__title">COVID-19 Dashboard</h1>
+    `;
 
     scheduleMain.innerHTML = `
         <div class="control-buttons">
@@ -38,10 +55,6 @@ function getDom(){
         </div>
     `;
 
-
-    document.body.append(scheduleMain);
-
-
     divSelect.append(choiceIndicator);
 
 
@@ -50,7 +63,10 @@ function getDom(){
     dataArticle.append(divSelect);
     dataArticle.append(divData);
 
-    document.body.append(dataArticle);
-    document.body.append(divStatistics);
+    wrapperInner.append(divStatistics,scheduleMain);
+    wrapper.append(map, wrapperInner)
 
+    root.append(dataArticle,wrapper)
+
+    document.body.append(header, root);
 }
